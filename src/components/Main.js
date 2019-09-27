653ea083d79fea62800fc577003f038a86d83806
Kyle Kelso <kyle.kelso@outlook.com>
2019-09-27 09:25:53 -0400

    Saving updates (returning after awhile)

--- src/components/Main.js
+++ src/components/Main.js
@@ -34,6 +34,13 @@ class Main extends React.Component {
         >
           {this.createCloseButton()}
           <h2>Projects</h2>
+          {/* multiple sections for each project. Show image and technology used in each one. */}
+          <section className="half">
+            {/* vote app */} Vote App Placeholder
+          </section>
+          <section className="half">
+            {/* twitter clone app */} Twitter Clone App Placeholder
+          </section>
         </div>
         <div
           id="about"
@@ -43,6 +50,12 @@ class Main extends React.Component {
         >
           {this.createCloseButton()}
           <h2>About</h2>
+          <section>
+            <p>
+              Hello! I am Kyle. I love to make websites and such! Feel free to
+              contact me about any job inquries!
+            </p>
+          </section>
         </div>
         <div
           id="contact"
@@ -52,6 +65,11 @@ class Main extends React.Component {
         >
           {this.createCloseButton()}
           <h2>Contact</h2>
+          <section>
+            <h4>Email: kyle.kelso@outlook.com</h4>
+            <h4>Github: https://github.com/kylekelso</h4>
+            <h4>LinkedIn: placeholder</h4>
+          </section>
         </div>
       </div>
     )
